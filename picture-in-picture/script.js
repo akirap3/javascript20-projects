006f9365047230ffa6073091e56d42590cf14aae
akirap3 <akirapf3@gmail.com>
2021-12-31 22:42:05 +0800

    Fix not show picture in picture

--- picture-in-picture/script.js
+++ picture-in-picture/script.js
@@ -1,25 +1,28 @@
 const videoElement = document.getElementById('video');
 const button = document.getElementById('button');
 
-// Prompt to select media stream, pass to video element, then play
+//promp to select media stream
 async function selectMediaStream() {
   try {
-    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
-    videoElement.srcObject = mediaStream;
+    videoElement.srcObject = await navigator.mediaDevices.getDisplayMedia({
+      video: true,
+    });
     videoElement.onloadedmetadata = () => {
       videoElement.play();
     };
   } catch (error) {
-    // Catch Error Here
-    console.log('whoops, error here:', error);
+    console.log(error);
   }
 }
 
 button.addEventListener('click', async () => {
+  //disable button
   button.disabled = true;
+  //start picture in picture
   await videoElement.requestPictureInPicture();
+  //reset button
   button.disabled = false;
 });
 
-// On Load
+//on load
 selectMediaStream();
